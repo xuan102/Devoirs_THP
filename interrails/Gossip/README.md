bc3f2f8fac9c1322b1d3f5c3daf971d01c8bb79c
Xuan102 <xuan102emma@gmail.com>
2020-03-14 12:56:57 +0100

    gossip

---
+++ interrails/Gossip/README.md
@@ -0,0 +1,21 @@
+Apres telecharger le dossier,sur terminal
+
+bundle install`
+
+### Database creation
+
+`rails db:create`
+`rails db:migrate`
+
+### Database initialisation
+
+`rails db:seed`
+
+## 🔥 Connexion au site
+
+`rails server`
+
+Une fois le server connecté, rendez-vous sur :
+
+http://localhost:3000/
+
---
+++ interrails/Gossip/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'pages#welcome'
+  get '/welcome/:first_name', to: 'pages#welcome'
+  get '/welcome', to: 'pages#welcome'
+  get '/equipe', to: 'pages#equipe'
+  get '/contacts', to: 'pages#contacts'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
